fa6709024e094c8b2cd3445c707923cd67b7a23b
ADMIN <bahiram.55pooja@gmail.com>
2026-08-19 23:45:16 +0530

    python operators

---
+++ operators.py
@@ -0,0 +1,14 @@
+# operators perform actions on values/variables
+# Arithmetic: +, -, *, /, //, %, **
+# Comparison: ==, !=, >, <, >=, <=
+# logical: and, or, not
+# Assignment: =, +=, -=, /=
+
+a = 10
+b = 3
+
+print(a + b)    # 13
+print(a // b)   # 3 floor division
+print(a % b)    # 1 reminder
+print(a ** b)   # 1000 power
+print(a > b > 0) # True
